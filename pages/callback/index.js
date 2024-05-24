152adc81553925ed8b565b86afeeb64659085a01
Myint Myat Thein <89845974+myintmyatthein403@users.noreply.github.com>
2024-05-24 22:46:29 +0700

    add: callback url

---
+++ pages/callback/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Callback() {
+    return (
+        <div>
+            <h1>Callback</h1>
+        </div>
+    )
+}
---
+++ pages/dev/callback/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Callback() {
+    return (
+        <div>
+            <h1>Dev Callback</h1>
+        </div>
+    )
+}
